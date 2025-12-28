1a5fda256f7f69df4050877b740d49cfd1eb21d1
Afnan Sayed <afnansayed1973@gimal.com>
2025-12-28 22:37:23 +0600

    prisma blog app

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Prisma Blog App!");
+});
+
+export default app;
+
