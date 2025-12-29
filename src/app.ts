2c95917cd60fd289170d8a496405ca3dd2a355d5
Afnan Sayed <afnansayed1973@gimal.com>
2025-12-29 14:36:15 +0600

    Email authenticaion to send email for verification

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express, { Application } from "express";
+import cors from "cors";
 import { postsRouter } from "./modules/posts/posts.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:3000",
+    credentials: true
+}))
 
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Prisma Blog App!");
 });
+
 
 app.use('/api/v1', postsRouter);
 
