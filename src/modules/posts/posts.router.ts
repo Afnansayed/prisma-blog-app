678c42b871605adec93c51765d8a1d5a9a857c1a
Afnan Sayed <afnansayed1973@gimal.com>
2025-12-31 12:13:22 +0600

    post router

--- src/modules/posts/posts.router.ts
+++ src/modules/posts/posts.router.ts
@@ -1,12 +1,16 @@
-import express from 'express';
-import { postController } from './posts.controller';
-
+import express, { NextFunction, Request, Response } from "express";
+import { postController } from "./posts.controller";
 
 const router = express.Router();
 
+const auth = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    console.log("middleware !!!!!!!!");
+    next();
+  };
+};
 
-router.post('/posts', postController.createPost);
-router.get('/posts', postController.getAllPosts);
-
+router.post("/posts", auth(), postController.createPost);
+router.get("/posts", postController.getAllPosts);
 
 export const postsRouter = router;
