cc7c905092e8c553c72da4cf1b712d74adc21d81
Afnan Sayed <afnansayed1973@gimal.com>
2026-01-11 11:47:54 +0600

    get comment by id and get comment by author id

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -19,7 +19,43 @@ const createComment = async (req: Request, res: Response) => {
     });
   }
 };
+const getCommentById = async (req: Request, res: Response) => {
+  if (!req.params.commentId) {
+    return res.status(400).json({
+      message: "Comment Id is required",
+    });
+  }
+
+  try {
+    const result = await commentsService.getCommentById(req.params.commentId as string);
+    res.status(200).json({
+      message: "Comment fetched successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
+};
+
+
+const getCommentsByAuthor = async (req: Request, res: Response) => {
+    try {
+        const { authorId } = req.params
+        const result = await commentsService.getCommentsByAuthor(authorId as string)
+        res.status(200).json(result)
+    } catch (e) {
+        res.status(400).json({
+            error: "Comment fetched failed",
+            details: e
+        })
+    }
+}
 
 export const commentsController = {
   createComment,
+  getCommentById,
+  getCommentsByAuthor
 };
--- src/modules/comments/comments.router.ts
+++ src/modules/comments/comments.router.ts
@@ -5,7 +5,12 @@ import { commentsController } from './comments.controller';
 
 const router = express.Router();
 
+router.get(
+    "/comments/author/:authorId",
+    commentsController.getCommentsByAuthor
+)
 router.post('/comments', auth(UserRole.USER , UserRole.ADMIN), commentsController.createComment);
+router.get('/comments/:commentId', commentsController.getCommentById);
 
 export const commentsRouter = router;
 
